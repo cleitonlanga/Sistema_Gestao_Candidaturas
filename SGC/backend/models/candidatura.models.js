569fdddd5a3107a59e30cd70c5f3cb1a9e01ab29
cleitonlanga <>
2025-11-24 23:10:40 +0200

    small changes on models

--- SGC/backend/models/candidatura.models.js
+++ SGC/backend/models/candidatura.models.js
@@ -10,7 +10,8 @@ export const criarTabelaCandidaturas = async () => {
       data_candidatura DATE NOT NULL,
       status VARCHAR(50) NOT NULL CHECK (status IN ('Enviada', 'Em Análise', 'Entrevista Agendada', 'Aguardando Retorno', 'Aprovada', 'Rejeitada')),
       salario VARCHAR(100),
-      plataforma VARCHAR(100),
+      plataforma VARCHAR(100) NOT NULL CHECK (plataforma IN ('LinkedIn', 'Link externo', 'Outro')),
+      link_plataforma VARCHAR(255),
       observacoes TEXT,
       data_criacao TIMESTAMP DEFAULT NOW(),
       data_atualizacao TIMESTAMP DEFAULT NOW()
